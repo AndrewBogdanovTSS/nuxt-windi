aa06ab880e828ddf6106fda46de97c1c9c6895b9
Andrew <andreybogdanov88@gmail.com>
2021-07-19 21:46:38 +0300

    initial commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="container-wrapper container mx-auto pl-4 bg-red-100">
+  <div class="container-wrapper my-container bg-red-200">
     <div>
       <Logo />
       <h1 class="">nuxt-windi</h1>
-      <div class="after:(content bg-red w-10 h-10)">
+      <div class="after:(content block bg-red-100 w-10 h-10)">
         <a href="https://nuxtjs.org/" target="_blank" rel="noopener noreferrer" class="button--green">
           Documentation
         </a>
@@ -14,11 +14,3 @@
     </div>
   </div>
 </template>
-
-<style>
-  @layer base {
-    .container-wrapper {
-      @apply bg-green-100;
-    }
-  }
-</style>
---
+++ windi.config.ts
@@ -0,0 +1,4 @@
+// windi.config.ts
+import { defineConfig } from "windicss/helpers";
+
+export default defineConfig({});
